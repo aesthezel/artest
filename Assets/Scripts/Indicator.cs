3f7a19453dd0cdf2c1308e7b8016a2c8158f40dd
David Pino <aesthezel@gmail.com>
2020-09-10 13:01:55 -0400

    New project, updated to Unity 2019.4.9f1

---
+++ Assets/Scripts/Indicator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Indicator : MonoBehaviour
+{
+    private ARRaycastManager rayMan;
+    private GameObject indicator;
+
+    void Start()
+    {
+        // Cargar los componentes
+        rayMan = FindObjectOfType<ARRaycastManager>();
+        indicator = transform.GetChild(0).gameObject;
+
+        // Ocultar el indicador
+        indicator.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Lanzar un raycast al centro de la pantalla
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayMan.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // Cuando un rayo toca el plano del suelo, actualizar la posición y rotación
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if(!indicator.activeInHierarchy)
+                indicator.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject building;
+    private Indicator spawnMark;
+
+    void Start()
+    {
+        spawnMark = FindObjectOfType<Indicator>();
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(building, spawnMark.transform.position, spawnMark.transform.rotation);
+        }
+    }
+
+}
